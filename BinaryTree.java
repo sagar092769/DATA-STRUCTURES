7d336b6ac6e6de83de070b5a1354904b687398aa
Sagar Choudhary <54681954+sagar927@users.noreply.github.com>
2019-11-20 08:10:38 +0530

    Add files via upload

---
+++ BinaryTree.java
@@ -0,0 +1,126 @@
+package CU;
+
+import java.util.Scanner;
+
+public class BinaryTree {
+
+	Scanner scn = new Scanner(System.in);
+
+	private class Node {
+
+		int data;
+		Node left;
+		Node right;
+	}
+
+	private Node root;
+
+	public BinaryTree() {
+		root = construct(null, false);
+	}
+
+	private Node construct(Node parent, boolean ilc) {
+
+		if (parent == null) {
+			System.out.println("Enter the data for root node ?");
+		} else {
+
+			if (ilc) {
+				System.out.println("Enter the data for left child of " + parent.data);
+			} else {
+				System.out.println("Enter the data for right child of " + parent.data);
+			}
+		}
+
+		int item = scn.nextInt();
+
+		Node nn = new Node();
+		nn.data = item;
+
+		System.out.println(nn.data + " has left child ?");
+		boolean hlc = scn.nextBoolean();
+
+		if (hlc) {
+			nn.left = construct(nn, true);
+		}
+
+		System.out.println(nn.data + " has right child ?");
+		boolean hrc = scn.nextBoolean();
+
+		if (hrc) {
+			nn.right = construct(nn, false);
+		}
+
+		return nn;
+	}
+
+	public void display() {
+		System.out.println("------------------");
+		display(root);
+		System.out.println("------------------");
+	}
+
+	private void display(Node node) {
+
+		if (node == null) {
+			return;
+		}
+
+		String str = "";
+
+		if (node.left == null) {
+			str += ".";
+		} else {
+			str += node.left.data;
+		}
+
+		str += " -> " + node.data + " <- ";
+
+		if (node.right == null) {
+			str += ".";
+		} else {
+			str += node.right.data;
+		}
+
+		System.out.println(str);
+
+		display(node.left);
+		display(node.right);
+
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node node) {
+
+		if (node == null) {
+			return 0;
+		}
+
+		int ls = size(node.left);
+		int rs = size(node.right);
+
+		return ls + rs + 1;
+	}
+
+
+	public int ht() {
+		return ht(root);
+	}
+
+	private int ht(Node node) {
+
+		if (node == null) {
+			return -1;
+
+		}
+		int lh = ht(node.left);
+		int rh = ht(node.right);
+
+		return Math.max(lh, rh) + 1;
+
+	}
+	
+}
